b5e4668ec0971311c676cf48c597d89f580abf66
Eliáš Sidon <sidonismo@gmail.com>
2021-11-30 01:51:38 +0100

    bez vyhledávání

--- ulozVyhledavani.js
+++ ulozVyhledavani.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://antikvariat11.cz/*
+// @match        https://antikvariat11.cz/kniha/*
 // @match        http://localhost:8000/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -12,9 +12,7 @@
 
 (function() {
     'use strict';
-        let searchbox = document.querySelector("#searchbox");
-    let sbtn = document.querySelector("#sbtn");
-    sbtn.addEventListener('click', function(event) {
-        localStorage.setItem("urlAdresa", searchbox.value);
-});
+    let vse = document.querySelector('table').innerText;
+let rok = vse.match(/\d{4}/)[0];
+        localStorage.setItem("rok", rok);
 })();
