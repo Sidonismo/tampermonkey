76e81460949fcc226db1cc9526e5ab1e962f6f2e
Eliáš Sidon <sidonismo@gmail.com>
2021-11-30 16:33:15 +0100

    nove knihy

--- iframe.js
+++ iframe.js
@@ -15,11 +15,20 @@
         var ifrm = document.createElement("iframe");
         ifrm.setAttribute("name", "display");
         ifrm.setAttribute("src", url);
-        ifrm.setAttribute("style", "bottom: 0px; left: 0px; width: 100%; height: 350px; position: fixed; background-color: white;");
+        ifrm.setAttribute("style", "bottom: 0px; left: 0px; width: 100%; height: 300px; position: fixed; background-color: white;");
         document.body.appendChild(ifrm);
     }
     console.log('Začátek');
         let nazev = document.querySelector('#sug_inp').value;
+    let rok = document.querySelectorAll('input');
+    let vse = "";
+    rok.forEach(ziskejRok);
+
+    function ziskejRok(item) {
+  vse += item.value;
+}
+    console.log(vse);
+    rok = vse.match(/[12][09]\d{2}/gm)[0];
     if (nazev.match(/\d+Svazky\s(\d*)/gmiu)){
         let svazky = nazev.match(/\d+Svazky\s(\d*)/gmiu)[0];
         nazev = nazev.replace(svazky, '');
@@ -32,7 +41,7 @@
     let autor = document.querySelector('#ed_i2').value;
 
     autor = autor.split(' ').pop();
-        let rok = localStorage.getItem('rok');
+
     let dotaz = 'https://exlibri.cz/prohledat-web/' + nazev + ' ' + autor + ' ' + rok;
     console.log(dotaz);
 
