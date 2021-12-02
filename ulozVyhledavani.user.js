fc7bea45a4fa5a920df42c68de270f048400892a
Eliáš Sidon <sidonismo@gmail.com>
2021-12-02 06:33:36 +0100

    Update ulozVyhledavani.user.js

--- ulozVyhledavani.user.js
+++ ulozVyhledavani.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ulozVyhledavani
 // @namespace    https://github.com/Sidonismo/tampermonkey/raw/main/ulozVyhledavani.user.js
-// @version      0.2
+// @version      0.2.2
 // @description  try to take over the world!
 // @author       Eliáš Sidon
 // @match        https://antikvariat11.cz/kniha/*
