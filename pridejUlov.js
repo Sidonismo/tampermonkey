d13412fee436de89d2ddaff10a09356e55e26315
Eliáš Sidon <sidonismo@gmail.com>
2023-12-13 18:11:06 +0100

    Create pridejUlov.js

---
+++ pridejUlov.js
@@ -0,0 +1,56 @@
+      // ==UserScript==
+// @name         pridejUlov
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://antikvariat11.cz/pridat-predmet*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+   const newButton = document.createElement("button");
+    const elem = document.querySelector("#content");
+    newButton.textContent = "Ulov";
+    newButton.addEventListener("click", myFunction);
+    let theFirstChild = elem.firstChild;
+    elem.insertBefore(newButton, theFirstChild);
+    const putAutor = document.querySelector("#ed_i2");
+    const putTitel = document.querySelector("#sug_inp");
+    const putIsbn = document.querySelector("#ed_i5");
+    const putRok = document.querySelector("#ed_i4");
+    const putStran = document.querySelector("#ed_i6");
+    const putCena = document.querySelector("#ed_i10");
+    console.log(newButton);
+    function myFunction() {
+      navigator.clipboard
+        .readText()
+        .then((text) => {
+          console.log(text);
+
+     var autorMatch = "";
+if (autorMatch = text.match(/\n.+(?=\/)/m)){
+autorMatch = text.match(/\n.+(?=\/)/m)[0];
+} else {
+    autorMatch = text.match(/\n.*/m)
+      }
+          console.log(autorMatch);
+
+          var titelMatch = text.match(/^(.*)$/m)[0];
+
+          let rokMatch = "";
+          rokMatch = titelMatch.match(/(?<=\()\d+/m);
+
+          putAutor.value = autorMatch;
+          putTitel.value = titelMatch.replace(/\(\d+\)/,'');
+          putRok.value = rokMatch;
+
+          putCena.value = "0";
+        })
+        .catch((err) => {
+          console.log("Failed to read clipboard contents: " + err);
+        });
+    }
+})();
