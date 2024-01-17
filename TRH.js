b1f9f4f90ac7909d75916310537b6910ad1237fc
Eliáš Sidon <sidonismo@gmail.com>
2024-01-17 17:16:32 +0100

    Update TRH.js

--- TRH.js
+++ TRH.js
@@ -7,7 +7,6 @@
 // @match        https://www.trhknih.cz/kniha/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=w3schools.com
 // @grant        none
-// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 
 
 
@@ -17,23 +16,28 @@
 
 (function() {
     'use strict';
+    const newButton = document.createElement("button");
+        const elem = document.querySelector(".btn-group");
+        newButton.textContent = "Kopíruj data";
+        newButton.addEventListener("click", myFunction);
+        let theFirstChild = elem.firstChild;
+    let sp1 = document.createElement("br");
 
+    elem.insertBefore(newButton, theFirstChild);
+    newButton.classList.add('btn','btn-primary','popover-link');
+        elem.after(sp1, newButton);
 
-  // Alert the copied text
+let autor = '';
+  function myFunction() {
   let titel = document.querySelector('h1').innerText;
-   let autor = [];
-   let autorArr = $('a[href^="/autor/"]');
-   $(autorArr).map(function(item, value) {
-       console.log(item);
-       console.log(value.innerText);
-       autor.push(value.innerText);
-       console.log(autor);
-   })
-    console.log(autor.toString());
-   let table = titel + "\n " + autor.toString().replace(',',', ') + "\n " + $('#basic-table').text();
+  if (document.querySelector("body > div.container > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p > a")) {
+      autor = document.querySelector("body > div.container > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p > a").innerText;
+  }
+  
+  console.log(autor.toString());
+  let table = titel + "\n " + autor.toString().replace(',',', ') + "\n " + $('#basic-table').text();
 
 
   navigator.clipboard.writeText(table);
-
-
+  }
 })();
